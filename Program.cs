6cadb8f0a79e96b7b9e67e2a4d380e95503644b6
Jessica Gonzales <jgonzales0688@gmail.com>
2023-01-11 18:44:41 -0600

    added weather api

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json.Linq;
 
 HttpClient client = new HttpClient(); //this allows us to make the requests
 
-
+Console.WriteLine("************** Exercise 1 ****************");
 string ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 string ronResponse = client.GetStringAsync(ronSwansonURL).Result;
 
@@ -25,3 +25,16 @@ for (int i = 0; i < 5; i++)
     Console.WriteLine();
     Console.WriteLine(kanyeObject);
 }
+
+Console.WriteLine("************** Exercise 2 ****************");
+
+Console.WriteLine("Please enter a city: ");
+var cityName = Console.ReadLine();
+
+var apiKey = "93007e58e0bbde76562e598a10e07b40";
+
+string weatherByCityURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial ";
+string weatherResponse = client.GetStringAsync(weatherByCityURL).Result;
+var currWeather = JObject.Parse(weatherResponse);
+
+Console.WriteLine($"The current temperature in {cityName} is {currWeather["main"]["temp"]} degrees.");
